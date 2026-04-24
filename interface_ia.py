917609e04e54a4635c46637c2d3561be97a00f26
neuralmindstudio-rgb <neuralmindstudio@gmail.com>
2026-04-24 16:54:48 -0300

    Update interface_ia.py

--- interface_ia.py
+++ interface_ia.py
@@ -124,7 +124,7 @@ class TelaPrincipal(Screen):
             size_hint=(1, None),
             height=dp(110), # Aumentei a altura total da barra
             spacing=dp(10),
-            padding=[dp(10), dp(80), dp(10), dp(5)], # Padding de 70 empurra os botões pra baixo
+            padding=[dp(10), dp(70), dp(10), dp(5)], # Padding de 70 empurra os botões pra baixo
             pos_hint={'top': 1}
         )
 
@@ -146,8 +146,8 @@ class TelaPrincipal(Screen):
         # --- ÁREA CENTRAL (Centralizada e Reduzida para 48%) ---
         self.meio = MDBoxLayout(
             orientation='vertical',
-            size_hint=(0.98, 0.62), # Foto menor para não bater em nada
-            pos_hint={'center_x': 0.5, 'center_y': 0.56}, # Bem no meio da tela
+            size_hint=(0.98, 0.75), # Foto menor para não bater em nada
+            pos_hint={'center_x': 0.5, 'center_y': 0.50}, # Bem no meio da tela
             md_bg_color=(0, 0, 0, 0),
             padding=dp(2)
         )
