ac02736ea270338a80dc6cc247f25b4e5b047a7c
neuralmindstudio-rgb <neuralmindstudio@gmail.com>
2026-04-28 13:18:19 -0300

    Update interface_ia.py

--- interface_ia.py
+++ interface_ia.py
@@ -135,7 +135,7 @@ class TelaPrincipal(Screen):
         # --- ÁREA CENTRAL ---
         self.meio = MDBoxLayout(
             orientation='vertical',
-            size_hint=(0.98, 0.56), 
+            size_hint=(0.98, 0.65), 
             pos_hint={'center_x': 0.5, 'center_y': 0.58}, 
             md_bg_color=(0, 0, 0, 0),
             padding=dp(2)
