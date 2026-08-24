2bbb6d4a0cec234d35dba369c0cfcbec49f2a770
neuralmindstudio-rgb <neuralmindstudio@gmail.com>
2026-08-24 14:31:58 -0300

    Update interface_ia.py [skip ci]

--- interface_ia.py
+++ interface_ia.py
@@ -174,7 +174,7 @@ class TelaPrincipal(Screen):
         def ajustar_area_central(*args):
             margem_lateral = dp(4)
             margem_topo = dp(4)
-            margem_baixo = dp(0)
+            margem_baixo = -dp(25)
 
             largura = max(dp(120), Window.width - (margem_lateral * 2))
             y_baixo = self.painel.y + self.painel.height + margem_baixo
